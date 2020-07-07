c9d61d31666bfdf100c1c5e6e7fb330218c40df4
Jecky Pei <jeckypei@163.com>
2020-07-07 14:04:02 +0800

    ix --> loc

--- tushare/stock/classifying.py
+++ tushare/stock/classifying.py
@@ -13,7 +13,7 @@ from tushare.stock import cons as ct
 from tushare.stock import ref_vars as rv
 import json
 import re
-from pandas.util.testing import _network_error_classes
+#from pandas.util.testing import _network_error_classes
 import time
 import tushare.stock.fundamental as fd
 from tushare.util.netbase import Client
@@ -197,9 +197,9 @@ def _get_detail(tag, retry_count=3, pause=0.001):
                                                                    p,tag))
                 text = urlopen(request, timeout=10).read()
                 text = text.decode('gbk')
-            except _network_error_classes:
-                pass
-            else:
+            #except _network_error_classes:
+            #    pass
+            finally:
                 break
         reg = re.compile(r'\,(.*?)\:')
         text = reg.sub(r',"\1":', text)
